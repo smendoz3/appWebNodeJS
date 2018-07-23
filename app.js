251abe664ccc752434e5bb790e0217732a8f0e4b
Santiago Mendoza <smendoz3@eafit.edu.co>
2018-07-22 19:34:40 -0500

    asd

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var config = require('./config/config');
+var glob = require('glob');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//Connect to MongoDB
+mongoose.connect(config.db);
+var db = mongoose.connection;
+
+//Handle MongoDB Error
+db.on('error', function () {
+    throw new Error('unable to connect to database at ' + config.db);
+});
+
+//use sessions for tracking logins
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+	mongooseConnection: db
+    })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// serve static files from template
+app.use(express.static(__dirname + '/templateLogReg'));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
+
+//Use MOdels
+var models = glob.sync(config.root + '/app/models/*.js');
+models.forEach(function (model) {
+    require(model);
+});
+
+module.exports = require('./config/express')(app, config);
+
+app.listen(config.port, function () {
+    console.log('Express server listening on port ' + config.port);
+});
